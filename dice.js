410d0a0aa26af120fdfd937e1765391e824801fc
Alan Jack <60137518+aldojack@users.noreply.github.com>
2021-11-23 22:41:19 +0000

    Add player class, DOM variables, random number function

--- dice.js
+++ dice.js
@@ -1,7 +1,34 @@
-let gameActive = false;
-let player = 0;
-let computer = 0;
+//Classes
+class player{
+    constructor(){
+        this.score = 0;
+         this.playerTurn = false;
+    }
+    addScore(dice){
+        this.score += dice;
+    }
+}
 
-/*
-    Look into creating object which player 1 and computer can share
-*/
+
+//DOM Variables
+const turnDisplay = document.getElementById('message');
+const player1Scoreboard = document.getElementById('player1Scoreboard');
+const player2Scoreboard = document.getElementById('player2Scoreboard');
+const player1Dice = document.getElementById('player1Dice');
+const player2Dice = document.getElementById('player2Dice');
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+
+//Declare variables
+
+let player1 = new player();
+let computer = new player();
+
+rollBtn.addEventListener('click',()=>{
+    const randomDice = rollDice();
+    player1Dice.textContent = dice
+})
+
+function rollDice(){
+    return Math.ceil(Math.random()*6);
+}
